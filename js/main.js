ef86b80081f76a955d43ce8dae6af0b68dbe76ea
mbellman <mbellman@nwe.com>
2016-04-21 05:10:21 -0700

    Some nice improvements to the map generator, including self-tiling!

--- js/main.js
+++ js/main.js
@@ -29,17 +29,29 @@ function main() {
 		var map = new HeightMap();
 		var its = 8;
 		var size = Math.pow(2, its) + 1;
-		var tile = Math.round(800 / size);
+		var tile = Math.round(500 / size);
+		var w = tile * size;
+
+		var t = Date.now();
+
 		map.generate({
 			iterations: its,
 			elevation: 100,
-			smoothness: 10
+			smoothness: 6,
+			repeat: true
 		});
+
+		console.log((Date.now() - t) + 'ms to generate');
+
 		map.scan(function(y, x, elevation){
 			var r = (elevation > 40 ? (elevation > 80 ? 155+elevation : 2*elevation) : 0);
 			var g = (elevation > 40 ? (elevation > 80 ? 120+elevation : 4*elevation) : elevation);
-			var b = (elevation > 40 ? (elevation > 80 ? 155+elevation : Math.round(elevation/2)) : 4*elevation);
+			var b = (elevation > 40 ? (elevation > 80 ? 155+elevation : Math.round(elevation/2)) : 135 + 3*elevation);
+
 			screen.draw.rectangle(10 + x*tile, 10 + y*tile, tile, tile).fill('rgb(' + r + ',' + g + ',' + b + ')');
+			screen.draw.rectangle(w + 10 + x*tile, 10 + y*tile, tile, tile).fill('rgb(' + r + ',' + g + ',' + b + ')');
+			screen.draw.rectangle(10 + x*tile, w + 10 + y*tile, tile, tile).fill('rgb(' + r + ',' + g + ',' + b + ')');
+			screen.draw.rectangle(w + 10 + x*tile, w + 10 + y*tile, tile, tile).fill('rgb(' + r + ',' + g + ',' + b + ')');
 		});
 	});
 
--- js/system.map.js
+++ js/system.map.js
@@ -5,10 +5,15 @@ function HeightMap() {
 	// Private:
 	var _ = this;
 	var seed = Date.now();
-	var settings = getDefaultSettings();
+	var settings = defaultSettings();
+	var mean = 0;
+	var distribution = 0;
 	var map = [];
 
-	function getDefaultSettings() {
+	/**
+	 * Default generator parameters
+	 */
+	function defaultSettings() {
 		return {
 			// Number of iterations for to subdivide the map
 			iterations: 4,
@@ -25,31 +30,76 @@ function HeightMap() {
 		};
 	}
 
+	/**
+	 * Negative-friendly modulus operation
+	 */
+	function mod(n, m) {
+		return ((n%m)+m)%m;
+	}
+
+	/**
+	 * A random decimal value from [0-1) cubed. Used to
+	 * skew the elevation sampler function toward the
+	 * mean by maintaining a smaller-tending deviation.
+	 */
+	function r3() {
+		var r = Math.random();
+		return r*r*r;
+	}
+
+	/**
+	 * Inclusive low <-> high random number generator
+	 */
+	function rand(low, high) {
+		return low + Math.floor(Math.random() * (high - low + 1));
+	}
+
+	/**
+	 * Keep a value within a certain range
+	 */
 	function clamp(value, min, max) {
 		return (value > max ? max : (value < min ? min : value));
 	}
 
-	function clampPoint(elevation) {
-		return clamp(elevation, 0, settings.elevation);
-	}
-
+	/**
+	 * Grab a point at coordinate [y, x]; wraps out-of-bounds coordinates
+	 */
 	function getPoint(y, x) {
-		if (y < 0) return getPoint(map.length - 1, x);
-		if (y >= map.length) return getPoint(0, x);
-		if (x < 0) return getPoint(y, map.length - 1);
-		if (x >= map.length) return getPoint(y, 0);
-		return map[y][x];
+		return map[mod(y,map.length)][mod(x,map.length)];
 	}
 
+	/**
+	 * Overwrite a null tile's elevation value. The condition
+	 * for null-only values is specified as not to overwrite
+	 * the initially generated random edges for tileable maps.
+	 */
+	function setPoint(y, x, value) {
+		if (map[y][x] === null) {
+			map[y][x] = clamp(value, 0, settings.elevation);
+		}
+	}
+
+	/**
+	 * Returns the average of a set of numbers
+	 */
 	function average(set) {
-		var sum = 0;
-		for (var i = 0 ; i < set.length ; i++) {
-			sum += set[i];
+		var sum = 0, i = 0;
+		while (typeof set[i] !== 'undefined') {
+			if (set[i] === null) {
+				set.splice(i, 1);
+				continue;
+			}
+			sum += set[i++];
 		}
 		return Math.round(sum / set.length);
 	}
 
-	function averageCorners(point, unit) {
+	/**
+	 * Returns the average elevation of four corners
+	 * from a central point {point} where each corner
+	 * is [unit] tiles away along the x and y axes
+	 */
+	function corners(point, unit) {
 		return average([
 			getPoint(point.y - unit, point.x - unit),
 			getPoint(point.y - unit, point.x + unit),
@@ -58,7 +108,11 @@ function HeightMap() {
 		]);
 	}
 
-	function averageSides(point, unit) {
+	/**
+	 * Returns the average elevation of four
+	 * adjacents next to a central point
+	 */
+	function adjacents(point, unit) {
 		return average([
 			getPoint(point.y, point.x - unit),
 			getPoint(point.y, point.x + unit),
@@ -67,19 +121,77 @@ function HeightMap() {
 		]);
 	}
 
-	function randomOffset(point, magnitude) {
-		return Math.round((Math.random()*settings.elevation) / (4*magnitude)) - Math.round(settings.smoothness/magnitude);
+	/**
+	 * Returns a randomly generated elevation value skewed
+	 * toward the mean as defined by the max elevation
+	 * and the 'average concentration %' parameter
+	 */
+	function sample() {
+		return clamp(mean + Math.round((rand(0,1) == 1 ? -r3()*distribution : r3()*distribution)), 0, settings.elevation);
 	}
 
-	function FlatTerrain(size) {
-		var terrain = new Array(size);
+	/**
+	 * Returns a random offset for rough surface formation,
+	 * taking the 'smoothness' parameter and magnitude, which
+	 * tapers off with fractal iterations, into account
+	 */
+	function offset(point, magnitude) {
+		magnitude = (magnitude < 1 ? 1 : magnitude);
+		return Math.round(rand(-8, 8) * settings.smoothness / magnitude);
+	}
+
+	/**
+	 * Returns a blank map array to be written to
+	 */
+	function emptyset(size) {
+		var set = new Array(size);
 		for (var i = 0 ; i < size ; i++) {
-			terrain[i] = new Array(size);
+			set[i] = new Array(size);
 			for (var j = 0 ; j < size ; j++) {
-				terrain[i][j] = 0;
+				set[i][j] = null;
 			}
 		}
-		return terrain;
+		return set;
+	}
+
+	/**
+	 * Returns a randomly generated 2D elevation line
+	 */
+	function randline(edgeValue) {
+		var line = new Array(Math.pow(2, settings.iterations) + 1);
+		line[0] = edgeValue;
+		line[line.length - 1] = edgeValue;
+		for (var step = 1 ; step <= settings.iterations ; step++) {
+			var unit = Math.pow(2, (settings.iterations-step));
+			var unit2 = unit * 2;
+			var points = Math.pow(2, (step-1));
+			for (var p = 0 ; p < points ; p++) {
+				var point = unit + unit2*p;
+				var left = line[point - unit];
+				var right = line[point + unit];
+				line[point] = average([left, right]) + offset(null, step);
+			}
+		}
+		return line;
+	}
+
+	/**
+	 * Generates a random edge for the top and left
+	 * sides of the map and 'stitches' them to the
+	 * opposite ends with slight height adjustments
+	 */
+	function wrap(cornerValue) {
+		var topline = randline(cornerValue);
+		var leftline = randline(cornerValue);
+		var size = map.length;
+		for (var p = 0 ; p < topline.length ; p++) {
+			setPoint(0, p, topline[p]);
+			setPoint(size-1, p, topline[p] + offset(null, settings.iterations-1));
+		}
+		for (var p = 0 ; p < leftline.length ; p++) {
+			setPoint(p, 0, leftline[p]);
+			setPoint(p, size-1, leftline[p] + offset(null, settings.iterations-1));
+		}
 	}
 
 	// Public:
@@ -100,49 +212,59 @@ function HeightMap() {
 
 		settings.iterations = clamp(settings.iterations, 1, 10);
 		settings.concentration = clamp(settings.concentration, 0, 100);
-		settings.smoothness = clamp(settings.smoothness, 1, 10);
+		settings.smoothness = clamp(settings.smoothness, 1, 20);
 		settings.erosion = clamp(settings.erosion, 0, 10);
 		settings.repeat = (typeof _settings.repeat === 'boolean' ? _settings.repeat : false);
 
-		// Generate fractal terrain
+		// Generate fractal terrain using diamond-square method
 		var size = Math.pow(2, settings.iterations) + 1;
-		var average = Math.round(settings.elevation * (settings.concentration / 100));
+		mean = Math.round(settings.elevation * (settings.concentration / 100));
+		distribution = Math.max(mean, settings.elevation - mean);
 
-		map = FlatTerrain(size);
-		map[0][0] = seed % average + randomOffset({y:0,x:0}, 1);
-		map[0][size - 1] = seed % average + randomOffset({y:0,x:size-1}, 1);
-		map[size - 1][0] = seed % average + randomOffset({y:size-1,x:0}, 1);
-		map[size - 1][size - 1] = seed % average + randomOffset({y:size-1,x:size-1}, 1);
+		map = emptyset(size);
+
+		if (settings.repeat) {
+			wrap(sample());
+		} else {
+			setPoint(0, 0, sample());
+			setPoint(0, size-1, sample());
+			setPoint(size-1, 0, sample());
+			setPoint(size-1, size-1, sample());
+		}
 
 		for (var step = 1 ; step <= settings.iterations ; step++) {
-			// Get subdivision size for this iteration
-			var unit = Math.pow(2, (settings.iterations - step));
+			// Get subdivided tile size for this iteration
+			var unit = Math.pow(2, (settings.iterations-step));
 			var unit2 = unit * 2;
-			var points = Math.pow(2, (step - 1));
+			var tiles = Math.pow(2, (step-1));
 
 			// Diamond (center elevation)
-			for (var y = 0 ; y < points ; y++) {
-				for (var x = 0 ; x < points ; x++) {
+			for (var y = 0 ; y < tiles ; y++) {
+				for (var x = 0 ; x < tiles ; x++) {
 					var center = {y: unit + unit2 * y, x: unit + unit2 * x};
-					var centerHeight = averageCorners(center, unit) + randomOffset(center, step);
+					var centerHeight = corners(center, unit) + offset(center, step);
 
-					map[center.y][center.x] = clampPoint(centerHeight);
+					if (Math.random() < 0.75 && step < 4) {
+						centerHeight = sample();
+					}
+
+					setPoint(center.y, center.x, centerHeight);
 				}
 			}
 
 			// Square (sides' elevation)
-			for (var y = 0 ; y < points ; y++) {
-				for (var x = 0 ; x < points ; x++) {
+			for (var y = 0 ; y < tiles ; y++) {
+				for (var x = 0 ; x < tiles ; x++) {
 					var center = {y: unit + unit2 * y, x: unit + unit2 * x};
 					var top = {y: center.y - unit, x: center.x};
 					var left = {y: center.y, x: center.x - unit};
 					var right = {y: center.y, x: center.x + unit};
 					var bottom = {y: center.y + unit, x: center.x};
 
-					map[top.y][top.x] = averageSides(top, unit) + randomOffset(top, step);
-					map[left.y][left.x] = averageSides(left, unit) + randomOffset(left, step);
-					map[right.y][right.x] = averageSides(right, unit) + randomOffset(right, step);
-					map[bottom.y][bottom.x] = averageSides(bottom, unit) + randomOffset(bottom, step);
+					setPoint(top.y, top.x, adjacents(top, unit) + offset(top, step));
+					setPoint(left.y, left.x, adjacents(left, unit) + offset(left, step));
+					setPoint(right.y, right.x, adjacents(right, unit) + offset(right, step));
+					setPoint(bottom.y, bottom.x, adjacents(bottom, unit) + offset(bottom, step));
 				}
 			}
 		}
@@ -151,8 +273,9 @@ function HeightMap() {
 	}
 
 	this.scan = function(handler) {
-		for (var y = 0 ; y < map.length ; y++) {
-			for (var x = 0 ; x < map.length ; x++) {
+		var size = map.length;
+		for (var y = 0 ; y < size ; y++) {
+			for (var x = 0 ; x < size ; x++) {
 				handler(y, x, map[y][x]);
 			}
 		}
